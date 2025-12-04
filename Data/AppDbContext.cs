55260a33d9823db595bd9378267af462426c299e
ameralmonajed <a.munajjed@icloud.com>
2025-12-04 01:27:47 +0100

    cretae EntityFramework

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
 using Restaurant_Menu.Models;
 
@@ -8,6 +9,13 @@ namespace Restaurant_Menu.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+            public DbSet<MenuItem> MenuItems =>  Set<MenuItem>();
+            public DbSet<Order> Orders => Set<Order>();
+            public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+
+            }
 
     
     }
